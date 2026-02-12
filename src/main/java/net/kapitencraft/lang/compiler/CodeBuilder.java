7676d3baecefd6e06b0d93aac59f17b8ef2374e6
Kapitencraft <kapitencraft@gmail.com>
2026-02-12 18:00:16 +0100

    started adding Bytecode optimization

---
+++ src/main/java/net/kapitencraft/lang/compiler/CodeBuilder.java
@@ -0,0 +1,10 @@
+package net.kapitencraft.lang.compiler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeBuilder {
+    private final List<Instruction> instructions = new ArrayList<>();
+
+    public void addInstruction
+}
