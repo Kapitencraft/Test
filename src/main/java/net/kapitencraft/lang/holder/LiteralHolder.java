72beafe72c6e7ae2eedccf7497f7691a3e1711c0
Kapitencraft <kapitencraft@gmail.com>
2026-07-09 13:32:41 +0200

    removed JSON IO from LiteralHolders

--- src/main/java/net/kapitencraft/lang/holder/LiteralHolder.java
+++ src/main/java/net/kapitencraft/lang/holder/LiteralHolder.java
@@ -1,13 +1,11 @@
 package net.kapitencraft.lang.holder;
 
-import com.google.gson.JsonObject;
 import net.kapitencraft.lang.holder.bytecode.const_pool.ConstantDoubleInfo;
 import net.kapitencraft.lang.holder.bytecode.const_pool.ConstantFloatInfo;
 import net.kapitencraft.lang.holder.bytecode.const_pool.ConstantIntegerInfo;
 import net.kapitencraft.lang.holder.bytecode.const_pool.ConstantPoolEntry;
-import net.kapitencraft.lang.run.VarTypeManager;
 import net.kapitencraft.lang.oop.clazz.ScriptedClass;
-import net.kapitencraft.tool.GsonHelper;
+import net.kapitencraft.lang.run.VarTypeManager;
 
 public record LiteralHolder(Object value, ScriptedClass type) {
 
@@ -26,35 +24,5 @@ public record LiteralHolder(Object value, ScriptedClass type) {
             return new ConstantIntegerInfo((Boolean) value ? 1 : 0);
         } else
             throw new IllegalArgumentException("unknown literal holder type: " + type);
-    }
-
-    public static LiteralHolder fromJson(JsonObject object) {
-        if (object.isEmpty()) return EMPTY;
-        String type = GsonHelper.getAsString(object, "type");
-        ScriptedClass target = switch (type) {
-            case "int" -> VarTypeManager.INTEGER;
-            case "float" -> VarTypeManager.FLOAT;
-            case "double" -> VarTypeManager.DOUBLE;
-            case "bool" -> VarTypeManager.BOOLEAN;
-            case "char" -> VarTypeManager.CHAR;
-            case "String" -> VarTypeManager.STRING.get();
-            default -> throw new IllegalArgumentException("unknown primitive type");
-        };
-        Object val = null;
-        if (object.has("value")) {
-            if (target == VarTypeManager.INTEGER) {
-                val = GsonHelper.getAsInt(object, "value");
-            } else if (target == VarTypeManager.FLOAT) {
-                val = GsonHelper.getAsFloat(object, "value");
-            } else if (target == VarTypeManager.DOUBLE) {
-                val = GsonHelper.getAsDouble(object, "value");
-            } else if (target == VarTypeManager.BOOLEAN) {
-                val = GsonHelper.getAsBoolean(object, "value");
-            } else if (target == VarTypeManager.CHAR) {
-                val = GsonHelper.getAsCharacter(object, "value");
-            } else
-                val = GsonHelper.getAsString(object, "value");
-        }
-        return new LiteralHolder(val, target);
     }
 }
